99f60ee40ca363e6262623af1fdc7ceeffb593ff
Fedor or Not <84645312+Hhk78@users.noreply.github.com>
2023-07-12 01:29:48 +0300

    Add files via upload

---
+++ install-cs-hl.sh
@@ -0,0 +1,7 @@
+curl -C - -L -O https://github.com/Hhk78/cs1.6-hl1-installer-for-linux/releases/download/31/cs.7z
+mkdir -p ~/.local/cs
+7z x cs.7z -o/home/$USER/.local/
+cp ./3 ~/Desktop/CS.desktop
+cp ./1 ~/Desktop/Half-Life.desktop
+cp ./3 ~/Masaüstü/CS.desktop
+cp ./1 ~/Masaüstü/Half-Life.desktop
